25f33283de166d944492fd0c27a69ebd120f05a1
eiden <v25024vv@aitech.ac.jp>
2026-03-21 02:46:16 +0900

    refactor(encounter): バックエンドのmy_id/target_id形式とGET /users/:id/encountersに合わせてリポジトリ・Notifier・UIを修正

--- lib/features/encounter/domain/encounter_model.freezed.dart
+++ lib/features/encounter/domain/encounter_model.freezed.dart
@@ -16,11 +16,11 @@ T _$identity<T>(T value) => value;
 mixin _$EncounterModel {
 
 /// すれ違い記録の一意なID
- String get id;/// すれ違ったユーザーの情報
- UserModel get encounteredUser;/// イベントID（イベント中のすれ違いの場合のみ）
- String? get eventId;/// すれ違った日時
- DateTime get encounteredAt;/// ユーザーがこのすれ違いを確認済みかどうか
- bool get isConfirmed;
+ String get id;/// すれ違ったユーザーの情報（JOINで取得された部分情報）
+ EncounteredUserInfo get encounteredUser;/// イベントID（イベント中のすれ違いの場合のみ）
+@JsonKey(name: 'event_id') int? get eventId;/// すれ違った日時
+@JsonKey(name: 'encountered_at') DateTime get encounteredAt;/// ユーザーがこのすれ違いを確認済みかどうか
+@JsonKey(name: 'is_confirmed') bool get isConfirmed;
 /// Create a copy of EncounterModel
 /// with the given fields replaced by the non-null parameter values.
 @JsonKey(includeFromJson: false, includeToJson: false)
@@ -53,11 +53,11 @@ abstract mixin class $EncounterModelCopyWith<$Res>  {
   factory $EncounterModelCopyWith(EncounterModel value, $Res Function(EncounterModel) _then) = _$EncounterModelCopyWithImpl;
 @useResult
 $Res call({
- String id, UserModel encounteredUser, String? eventId, DateTime encounteredAt, bool isConfirmed
+ String id, EncounteredUserInfo encounteredUser,@JsonKey(name: 'event_id') int? eventId,@JsonKey(name: 'encountered_at') DateTime encounteredAt,@JsonKey(name: 'is_confirmed') bool isConfirmed
 });
 
 
-$UserModelCopyWith<$Res> get encounteredUser;
+$EncounteredUserInfoCopyWith<$Res> get encounteredUser;
 
 }
 /// @nodoc
@@ -74,8 +74,8 @@ class _$EncounterModelCopyWithImpl<$Res>
   return _then(_self.copyWith(
 id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
 as String,encounteredUser: null == encounteredUser ? _self.encounteredUser : encounteredUser // ignore: cast_nullable_to_non_nullable
-as UserModel,eventId: freezed == eventId ? _self.eventId : eventId // ignore: cast_nullable_to_non_nullable
-as String?,encounteredAt: null == encounteredAt ? _self.encounteredAt : encounteredAt // ignore: cast_nullable_to_non_nullable
+as EncounteredUserInfo,eventId: freezed == eventId ? _self.eventId : eventId // ignore: cast_nullable_to_non_nullable
+as int?,encounteredAt: null == encounteredAt ? _self.encounteredAt : encounteredAt // ignore: cast_nullable_to_non_nullable
 as DateTime,isConfirmed: null == isConfirmed ? _self.isConfirmed : isConfirmed // ignore: cast_nullable_to_non_nullable
 as bool,
   ));
@@ -84,9 +84,9 @@ as bool,
 /// with the given fields replaced by the non-null parameter values.
 @override
 @pragma('vm:prefer-inline')
-$UserModelCopyWith<$Res> get encounteredUser {
+$EncounteredUserInfoCopyWith<$Res> get encounteredUser {
   
-  return $UserModelCopyWith<$Res>(_self.encounteredUser, (value) {
+  return $EncounteredUserInfoCopyWith<$Res>(_self.encounteredUser, (value) {
     return _then(_self.copyWith(encounteredUser: value));
   });
 }
@@ -171,7 +171,7 @@ return $default(_that);case _:
 /// }
 /// ```
 
-@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( String id,  UserModel encounteredUser,  String? eventId,  DateTime encounteredAt,  bool isConfirmed)?  $default,{required TResult orElse(),}) {final _that = this;
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( String id,  EncounteredUserInfo encounteredUser, @JsonKey(name: 'event_id')  int? eventId, @JsonKey(name: 'encountered_at')  DateTime encounteredAt, @JsonKey(name: 'is_confirmed')  bool isConfirmed)?  $default,{required TResult orElse(),}) {final _that = this;
 switch (_that) {
 case _EncounterModel() when $default != null:
 return $default(_that.id,_that.encounteredUser,_that.eventId,_that.encounteredAt,_that.isConfirmed);case _:
@@ -192,7 +192,7 @@ return $default(_that.id,_that.encounteredUser,_that.eventId,_that.encounteredAt
 /// }
 /// ```
 
-@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( String id,  UserModel encounteredUser,  String? eventId,  DateTime encounteredAt,  bool isConfirmed)  $default,) {final _that = this;
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( String id,  EncounteredUserInfo encounteredUser, @JsonKey(name: 'event_id')  int? eventId, @JsonKey(name: 'encountered_at')  DateTime encounteredAt, @JsonKey(name: 'is_confirmed')  bool isConfirmed)  $default,) {final _that = this;
 switch (_that) {
 case _EncounterModel():
 return $default(_that.id,_that.encounteredUser,_that.eventId,_that.encounteredAt,_that.isConfirmed);case _:
@@ -212,7 +212,7 @@ return $default(_that.id,_that.encounteredUser,_that.eventId,_that.encounteredAt
 /// }
 /// ```
 
-@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( String id,  UserModel encounteredUser,  String? eventId,  DateTime encounteredAt,  bool isConfirmed)?  $default,) {final _that = this;
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( String id,  EncounteredUserInfo encounteredUser, @JsonKey(name: 'event_id')  int? eventId, @JsonKey(name: 'encountered_at')  DateTime encounteredAt, @JsonKey(name: 'is_confirmed')  bool isConfirmed)?  $default,) {final _that = this;
 switch (_that) {
 case _EncounterModel() when $default != null:
 return $default(_that.id,_that.encounteredUser,_that.eventId,_that.encounteredAt,_that.isConfirmed);case _:
@@ -227,19 +227,19 @@ return $default(_that.id,_that.encounteredUser,_that.eventId,_that.encounteredAt
 @JsonSerializable()
 
 class _EncounterModel implements EncounterModel {
-  const _EncounterModel({required this.id, required this.encounteredUser, this.eventId, required this.encounteredAt, required this.isConfirmed});
+  const _EncounterModel({this.id = '', required this.encounteredUser, @JsonKey(name: 'event_id') this.eventId, @JsonKey(name: 'encountered_at') required this.encounteredAt, @JsonKey(name: 'is_confirmed') this.isConfirmed = false});
   factory _EncounterModel.fromJson(Map<String, dynamic> json) => _$EncounterModelFromJson(json);
 
 /// すれ違い記録の一意なID
-@override final  String id;
-/// すれ違ったユーザーの情報
-@override final  UserModel encounteredUser;
+@override@JsonKey() final  String id;
+/// すれ違ったユーザーの情報（JOINで取得された部分情報）
+@override final  EncounteredUserInfo encounteredUser;
 /// イベントID（イベント中のすれ違いの場合のみ）
-@override final  String? eventId;
+@override@JsonKey(name: 'event_id') final  int? eventId;
 /// すれ違った日時
-@override final  DateTime encounteredAt;
+@override@JsonKey(name: 'encountered_at') final  DateTime encounteredAt;
 /// ユーザーがこのすれ違いを確認済みかどうか
-@override final  bool isConfirmed;
+@override@JsonKey(name: 'is_confirmed') final  bool isConfirmed;
 
 /// Create a copy of EncounterModel
 /// with the given fields replaced by the non-null parameter values.
@@ -274,11 +274,11 @@ abstract mixin class _$EncounterModelCopyWith<$Res> implements $EncounterModelCo
   factory _$EncounterModelCopyWith(_EncounterModel value, $Res Function(_EncounterModel) _then) = __$EncounterModelCopyWithImpl;
 @override @useResult
 $Res call({
- String id, UserModel encounteredUser, String? eventId, DateTime encounteredAt, bool isConfirmed
+ String id, EncounteredUserInfo encounteredUser,@JsonKey(name: 'event_id') int? eventId,@JsonKey(name: 'encountered_at') DateTime encounteredAt,@JsonKey(name: 'is_confirmed') bool isConfirmed
 });
 
 
-@override $UserModelCopyWith<$Res> get encounteredUser;
+@override $EncounteredUserInfoCopyWith<$Res> get encounteredUser;
 
 }
 /// @nodoc
@@ -295,8 +295,8 @@ class __$EncounterModelCopyWithImpl<$Res>
   return _then(_EncounterModel(
 id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
 as String,encounteredUser: null == encounteredUser ? _self.encounteredUser : encounteredUser // ignore: cast_nullable_to_non_nullable
-as UserModel,eventId: freezed == eventId ? _self.eventId : eventId // ignore: cast_nullable_to_non_nullable
-as String?,encounteredAt: null == encounteredAt ? _self.encounteredAt : encounteredAt // ignore: cast_nullable_to_non_nullable
+as EncounteredUserInfo,eventId: freezed == eventId ? _self.eventId : eventId // ignore: cast_nullable_to_non_nullable
+as int?,encounteredAt: null == encounteredAt ? _self.encounteredAt : encounteredAt // ignore: cast_nullable_to_non_nullable
 as DateTime,isConfirmed: null == isConfirmed ? _self.isConfirmed : isConfirmed // ignore: cast_nullable_to_non_nullable
 as bool,
   ));
@@ -306,12 +306,284 @@ as bool,
 /// with the given fields replaced by the non-null parameter values.
 @override
 @pragma('vm:prefer-inline')
-$UserModelCopyWith<$Res> get encounteredUser {
+$EncounteredUserInfoCopyWith<$Res> get encounteredUser {
   
-  return $UserModelCopyWith<$Res>(_self.encounteredUser, (value) {
+  return $EncounteredUserInfoCopyWith<$Res>(_self.encounteredUser, (value) {
     return _then(_self.copyWith(encounteredUser: value));
   });
 }
+}
+
+
+/// @nodoc
+mixin _$EncounteredUserInfo {
+
+ String get id; String get name;@JsonKey(name: 'icon_url') String get iconUrl;@JsonKey(name: 'one_word') String get oneWord;
+/// Create a copy of EncounteredUserInfo
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$EncounteredUserInfoCopyWith<EncounteredUserInfo> get copyWith => _$EncounteredUserInfoCopyWithImpl<EncounteredUserInfo>(this as EncounteredUserInfo, _$identity);
+
+  /// Serializes this EncounteredUserInfo to a JSON map.
+  Map<String, dynamic> toJson();
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is EncounteredUserInfo&&(identical(other.id, id) || other.id == id)&&(identical(other.name, name) || other.name == name)&&(identical(other.iconUrl, iconUrl) || other.iconUrl == iconUrl)&&(identical(other.oneWord, oneWord) || other.oneWord == oneWord));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,name,iconUrl,oneWord);
+
+@override
+String toString() {
+  return 'EncounteredUserInfo(id: $id, name: $name, iconUrl: $iconUrl, oneWord: $oneWord)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $EncounteredUserInfoCopyWith<$Res>  {
+  factory $EncounteredUserInfoCopyWith(EncounteredUserInfo value, $Res Function(EncounteredUserInfo) _then) = _$EncounteredUserInfoCopyWithImpl;
+@useResult
+$Res call({
+ String id, String name,@JsonKey(name: 'icon_url') String iconUrl,@JsonKey(name: 'one_word') String oneWord
+});
+
+
+
+
+}
+/// @nodoc
+class _$EncounteredUserInfoCopyWithImpl<$Res>
+    implements $EncounteredUserInfoCopyWith<$Res> {
+  _$EncounteredUserInfoCopyWithImpl(this._self, this._then);
+
+  final EncounteredUserInfo _self;
+  final $Res Function(EncounteredUserInfo) _then;
+
+/// Create a copy of EncounteredUserInfo
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = null,Object? name = null,Object? iconUrl = null,Object? oneWord = null,}) {
+  return _then(_self.copyWith(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,name: null == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String,iconUrl: null == iconUrl ? _self.iconUrl : iconUrl // ignore: cast_nullable_to_non_nullable
+as String,oneWord: null == oneWord ? _self.oneWord : oneWord // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
+}
+
+}
+
+
+/// Adds pattern-matching-related methods to [EncounteredUserInfo].
+extension EncounteredUserInfoPatterns on EncounteredUserInfo {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _EncounteredUserInfo value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _EncounteredUserInfo() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _EncounteredUserInfo value)  $default,){
+final _that = this;
+switch (_that) {
+case _EncounteredUserInfo():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _EncounteredUserInfo value)?  $default,){
+final _that = this;
+switch (_that) {
+case _EncounteredUserInfo() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( String id,  String name, @JsonKey(name: 'icon_url')  String iconUrl, @JsonKey(name: 'one_word')  String oneWord)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _EncounteredUserInfo() when $default != null:
+return $default(_that.id,_that.name,_that.iconUrl,_that.oneWord);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( String id,  String name, @JsonKey(name: 'icon_url')  String iconUrl, @JsonKey(name: 'one_word')  String oneWord)  $default,) {final _that = this;
+switch (_that) {
+case _EncounteredUserInfo():
+return $default(_that.id,_that.name,_that.iconUrl,_that.oneWord);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( String id,  String name, @JsonKey(name: 'icon_url')  String iconUrl, @JsonKey(name: 'one_word')  String oneWord)?  $default,) {final _that = this;
+switch (_that) {
+case _EncounteredUserInfo() when $default != null:
+return $default(_that.id,_that.name,_that.iconUrl,_that.oneWord);case _:
+  return null;
+
+}
+}
+
+}
+
+/// @nodoc
+@JsonSerializable()
+
+class _EncounteredUserInfo implements EncounteredUserInfo {
+  const _EncounteredUserInfo({required this.id, this.name = '???', @JsonKey(name: 'icon_url') this.iconUrl = '', @JsonKey(name: 'one_word') this.oneWord = ''});
+  factory _EncounteredUserInfo.fromJson(Map<String, dynamic> json) => _$EncounteredUserInfoFromJson(json);
+
+@override final  String id;
+@override@JsonKey() final  String name;
+@override@JsonKey(name: 'icon_url') final  String iconUrl;
+@override@JsonKey(name: 'one_word') final  String oneWord;
+
+/// Create a copy of EncounteredUserInfo
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$EncounteredUserInfoCopyWith<_EncounteredUserInfo> get copyWith => __$EncounteredUserInfoCopyWithImpl<_EncounteredUserInfo>(this, _$identity);
+
+@override
+Map<String, dynamic> toJson() {
+  return _$EncounteredUserInfoToJson(this, );
+}
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _EncounteredUserInfo&&(identical(other.id, id) || other.id == id)&&(identical(other.name, name) || other.name == name)&&(identical(other.iconUrl, iconUrl) || other.iconUrl == iconUrl)&&(identical(other.oneWord, oneWord) || other.oneWord == oneWord));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,name,iconUrl,oneWord);
+
+@override
+String toString() {
+  return 'EncounteredUserInfo(id: $id, name: $name, iconUrl: $iconUrl, oneWord: $oneWord)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$EncounteredUserInfoCopyWith<$Res> implements $EncounteredUserInfoCopyWith<$Res> {
+  factory _$EncounteredUserInfoCopyWith(_EncounteredUserInfo value, $Res Function(_EncounteredUserInfo) _then) = __$EncounteredUserInfoCopyWithImpl;
+@override @useResult
+$Res call({
+ String id, String name,@JsonKey(name: 'icon_url') String iconUrl,@JsonKey(name: 'one_word') String oneWord
+});
+
+
+
+
+}
+/// @nodoc
+class __$EncounteredUserInfoCopyWithImpl<$Res>
+    implements _$EncounteredUserInfoCopyWith<$Res> {
+  __$EncounteredUserInfoCopyWithImpl(this._self, this._then);
+
+  final _EncounteredUserInfo _self;
+  final $Res Function(_EncounteredUserInfo) _then;
+
+/// Create a copy of EncounteredUserInfo
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = null,Object? name = null,Object? iconUrl = null,Object? oneWord = null,}) {
+  return _then(_EncounteredUserInfo(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,name: null == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String,iconUrl: null == iconUrl ? _self.iconUrl : iconUrl // ignore: cast_nullable_to_non_nullable
+as String,oneWord: null == oneWord ? _self.oneWord : oneWord // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
+}
+
+
 }
 
 // dart format on
--- lib/features/encounter/domain/encounter_model.g.dart
+++ lib/features/encounter/domain/encounter_model.g.dart
@@ -8,20 +8,37 @@ part of 'encounter_model.dart';
 
 _EncounterModel _$EncounterModelFromJson(Map<String, dynamic> json) =>
     _EncounterModel(
-      id: json['id'] as String,
-      encounteredUser: UserModel.fromJson(
+      id: json['id'] as String? ?? '',
+      encounteredUser: EncounteredUserInfo.fromJson(
         json['encounteredUser'] as Map<String, dynamic>,
       ),
-      eventId: json['eventId'] as String?,
-      encounteredAt: DateTime.parse(json['encounteredAt'] as String),
-      isConfirmed: json['isConfirmed'] as bool,
+      eventId: (json['event_id'] as num?)?.toInt(),
+      encounteredAt: DateTime.parse(json['encountered_at'] as String),
+      isConfirmed: json['is_confirmed'] as bool? ?? false,
     );
 
 Map<String, dynamic> _$EncounterModelToJson(_EncounterModel instance) =>
     <String, dynamic>{
       'id': instance.id,
       'encounteredUser': instance.encounteredUser,
-      'eventId': instance.eventId,
-      'encounteredAt': instance.encounteredAt.toIso8601String(),
-      'isConfirmed': instance.isConfirmed,
+      'event_id': instance.eventId,
+      'encountered_at': instance.encounteredAt.toIso8601String(),
+      'is_confirmed': instance.isConfirmed,
     };
+
+_EncounteredUserInfo _$EncounteredUserInfoFromJson(Map<String, dynamic> json) =>
+    _EncounteredUserInfo(
+      id: json['id'] as String,
+      name: json['name'] as String? ?? '???',
+      iconUrl: json['icon_url'] as String? ?? '',
+      oneWord: json['one_word'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$EncounteredUserInfoToJson(
+  _EncounteredUserInfo instance,
+) => <String, dynamic>{
+  'id': instance.id,
+  'name': instance.name,
+  'icon_url': instance.iconUrl,
+  'one_word': instance.oneWord,
+};
